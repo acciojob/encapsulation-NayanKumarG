c17fc95112d1c760293129c7d90d81b2ddfbcf2e
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-08-14 12:18:34 +0530

    Attempted encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        //r.name = "nayan";
+        //System.out.println(r.name);
+       //java: name has private access in com.driver.RWOnly
+        r.set("nayan");
+        String name = r.get();
+        System.out.println(name);
+
+    }
 }
